508e7578442f8f3a252c49c78b3943fc48ef370a
Desmond Odakpo-Oweibo <114571991+Brume001@users.noreply.github.com>
2023-04-08 19:12:34 -0700

    Update Info.jsx

--- src/component/about/Info.jsx
+++ src/component/about/Info.jsx
@@ -11,7 +11,7 @@ const Info = () => {
             <div className="about__box">
                 <i class='bx bx-briefcase-alt about__icon'></i>
                 <h3 className="about__title">Completed</h3>
-                <span className="about__subtitle">48 + Projects</span>
+                <span className="about__subtitle">22 + Projects</span>
             </div>
             <div className="about__box">
                 <i class='bx bx-support about__icon'></i>
